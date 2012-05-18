f2929848b56606f28590c10790063df4e338883a
Ivan Tkachenko <nelas.ya@gmail.com>
2012-05-18 14:56:05 +0400

    travis.ci added

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,14 @@
+module ApplicationHelper
+  def logo
+    @logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  # Return a title on a per-page basis.               # Documentation comment
+  def title                                           # Method definition
+    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if @title.nil?                                    # Boolean test for nil
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{@title}"                     # String interpolation
+    end
+  end
+end
